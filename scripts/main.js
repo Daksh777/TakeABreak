662fce74dc37c6791a938e20abb7a7e1df7c2895
Ahmed Akram <eng.akramovic@gmail.com>
2022-05-23 11:43:57 +0200

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1285,25 +1285,21 @@ function deleteTab(tab, tabLink) {
 
 // blob added
 
-// var pre_theme = localStorage.getItem("theme");
-// if (pre_theme === "colorful-theme") {
-//   document.getElementById("themeid").checked = true;
-// } else {
-//   document.getElementById("themeid").checked = false;
-// }
-// document.body.classList.toggle(pre_theme);
-// var themeid = document.getElementById("themeid");
-// themeid.onclick = function () {
-//   if ((document.getElementById("themeid").checked === true)) {
-//     localStorage.setItem("theme", "colorful-theme");
-//     document.body.classList.toggle("colorful-theme");
-//   } else {
-//     localStorage.removeItem("theme");
-//     document.body.classList.toggle("colorful-theme");
-//   }
-// };
+var pre_theme = localStorage.getItem("theme");
+if (pre_theme === "colorful-theme") {
+  document.getElementById("themeid").checked = true;
+} else {
+  document.getElementById("themeid").checked = false;
+}
+document.body.classList.toggle(pre_theme);
+var themeid = document.getElementById("themeid");
 themeid.onclick = function () {
-  if ((document.getElementById("themeid").checked === true)) 
-    document.body.classList.toggle("theme");
+  if ((document.getElementById("themeid").checked === true)) {
+    localStorage.setItem("theme", "theme");
+    document.body.classList.toggle("colorful-theme");
+  } else {
+    localStorage.removeItem("theme");
+    document.body.classList.toggle("colorful-theme");
+  }
 };
 
